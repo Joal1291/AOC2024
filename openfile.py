fdb883de36a29fcb80fdeaaa19d2e2627f99d8e8
Joal1291 <jordan.albert@github.com>
2024-12-03 08:39:13 +0100

    reinit

--- openfile.py
+++
@@ -1,9 +0,0 @@
-path = "C:/Users/jordy/Documents/Code/algorithmie/AOC2024/puzzle_input/"
-
-def readFile(filename: str):
-    fichier = open(path+filename, 'r')
-    words = []
-    for line in fichier:
-        words.extend(line.strip().split('\n'))
-    # print(words)
-    return words
