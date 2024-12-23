077cbd69518a174023d9bd0c20ca266a0ecb43a0
Joal1291 <jordan.albert@github.com>
2024-12-24 00:53:10 +0100

    day 7 OVER o yeah!!

--- day_7_part_2.py
+++ day_7_part_2.py
@@ -1,4 +1,4 @@
-from utils.openfile import readFile
+from openfile import readFile
 from itertools import product
 import re
 
@@ -33,61 +33,33 @@ def left_to_right(numbers, operateurs):
             result+= int(numbers[i+1])
         elif operateur == '*':
             result *= int(numbers[i+1])
+        elif operateur == '||': # ligne ajouter pour la logique de calcul...
+            result = int(str(result)+str(numbers[i+1]))
     return result
 
 def test_line(target, numbers):
-    operations= list(product(['*', '+'], repeat=len(numbers)-1))
-    count = 1
+    operations= list(product(['*', '+',  '||'], repeat=len(numbers)-1)) # pour le day 2 j'ai finalement juste ajouter l'opérateur '||' concat et une ligne dans left to right
     for operation in operations:
         result = left_to_right(numbers, operation)
-        print(f"operation n° {count} avec les operateur {operation}")
-        print(result)
-        print("----")
         if result == target:
             return True
-        count =+1
-    return False
-
-# Pour la seconde partie du jour 7 je vais juste ajouter une function qui vas prendre les ligne
-# qui n'ont pas passer le premier test puis je vais créer un tableau dans le quelle sera inserer 
-# toutes les nouvelle ligne qui auront concatener les deux nombre cote a cote
-# exception fait des tableau avec just deux nombre qui sera directement concatenenr et tester
-
-def find_new_line(target, numbers):
-    if len(numbers) == 2:
-        if target == int(str(numbers[0])+str(numbers[1])):
-            return True
-    else:
-        array_of_new_line = []
-        count=0
-        for i in range(0, len(numbers)-1):
-            tampon = []
-            for j in range(0, len(numbers)):
-                if j == count:
-                    tampon.append(str(numbers[i])+str(numbers[i+1]))
-                elif j == count+1:
-                    continue
-                else:
-                    tampon.append(numbers[j])
-            array_of_new_line.append(tampon)
-            count+=1
-        print(array_of_new_line)
-        count = 1
-        for i in array_of_new_line:
-            print(f"---------- {count} ------------")
-            if test_line(target, i):
-                return True
-            count+= 1
-
     return False
 
 result = 0         
-# for i in test:
-target, numbers = get_utils(test[4])
-if test_line(target, numbers):
-    result += target
-elif find_new_line(target, numbers):
-    result += target
-    
+for i in test:
+    target, numbers = get_utils(i)
+    if test_line(target, numbers):
+        result += target 
 
 print(result)
+
+# Ok j'ai passe le test cependant le processus à pris 21 secondes ce qui est beaucoup trop 
+# je pense que cela est du au fait qu'il test des ligne inutile 
+# il faudrais pouvoir créer des ligne qui ne contienne pas deux ou trois fois l'opérateur concat
+# car avec cette solution les ligne peuvent contenir plus de deux fois l'opéarateur concat alors 
+# que je dois tester qu'une seule fois l'opérateur par ligne exemple pour une ligne tel que 
+# 15 12 16 
+# il doit tester 15 (* et +) 12 || 16
+# puis 15 || 12 (* et +) 16
+# est la solution actuel propose aussi cette ligne 15 || 12 || 16
+# ce qui n'est pas demander du tout
